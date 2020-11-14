7dff3ab13a23a1672b985807900720e087d0fc82
Shay9 <72321250+Shay9@users.noreply.github.com>
2020-11-14 16:47:51 -0500

    temperature and elements added

---
+++ SheCodes Plus Final Project/src/Index.js
@@ -0,0 +1,21 @@
+function showTemperature (response){
+    console.log(response);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name
+    descriptionElement.innerHTML = response.data.weather[0].description
+    humidityElement.innerHTML = response.data.main.humidity
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let city = "London";
+let apiKey = "a35a6fc9aa256480d5ede5662dad54ec";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(showTemperature);
