5d2591c0b9c466464089a7f1683aa69ef92062a8
Shay9 <72321250+Shay9@users.noreply.github.com>
2020-11-14 20:04:36 -0500

    icon api added, css updated

--- SheCodes Plus Final Project/src/Index.js
+++ SheCodes Plus Final Project/src/Index.js
@@ -1,3 +1,19 @@
+
+function date (timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10){
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10){
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day},  ${hours}:${minutes}`;
+}
+
 function showTemperature (response){
     console.log(response);
     let temperatureElement = document.querySelector("#temperature");
@@ -5,15 +21,22 @@ function showTemperature (response){
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let datetimeElement = document.querySelector("#datetime");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name
     descriptionElement.innerHTML = response.data.weather[0].description
     humidityElement.innerHTML = response.data.main.humidity
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    datetimeElement.innerHTML = date(response.data.dt * 1000);
+     iconElement.setAttribute("src", 
+     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+     iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
 
-let city = "London";
+let city = "san fransisco";
 let apiKey = "a35a6fc9aa256480d5ede5662dad54ec";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
